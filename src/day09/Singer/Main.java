16c206c592490acc792e896edd61f92bb91da7d2
kimjieung97 <dickseung23@naver.com>
2023-04-06 23:20:09 +0900

    노래관리프로그램 복습

--- src/day09/Singer/Main.java
+++ src/day09/Singer/Main.java
@@ -2,8 +2,7 @@ package day09.Singer;
 
 public class Main {
     public static void main(String[] args) {
-        ArtistView atvArtistView = new ArtistView();
 
-        atvArtistView.showmenu();
+        ArtistView.start();
     }
 }
--- src/day09/Singer/Test.java
+++ src/day09/Singer/Test.java
@@ -1,9 +1,15 @@
 package day09.Singer;
 
+import day04.array.StringList;
+
 public class Test {
     public static void main(String[] args) {
         //가수 객체 생성
+        Artist kim = new Artist("동방신기", new StringList("hug"));
 
-        Artist kim = new Artist();
+        StringList songList = kim.getSongList();
+
+        kim.info();
+
     }
 }
