36f446268680b0bfbc8818d4b59c7551491666a4
jisuengkim <dickseung23@naver.com>
2023-04-07 17:50:41 +0900

    가수 등록프로그램 수정

--- src/day09/Singer/Test.java
+++ src/day09/Singer/Test.java
@@ -2,6 +2,9 @@ package day09.Singer;
 
 import day04.array.StringList;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class Test {
     public static void main(String[] args) {
         //가수 객체 생성
@@ -10,6 +13,10 @@ public class Test {
         StringList songList = kim.getSongList();
 
         kim.info();
+
+        String [] str =  songList.getsArr();
+
+        System.out.println(Arrays.toString(str));
 
     }
 }
